eb315213511f0d7044eefdf7e3d837d7c56a6354
Yuri <yv@compell.io>
2022-03-29 16:38:41 +0300

    Basic contract interaction

--- schemaRegistry.py
+++ schemaRegistry.py
@@ -3,36 +3,70 @@
 import smartpy as sp
 
 class SchemaRegistry(sp.Contract):
-    def __init__(self, schema_id, logic_contract_address):
+    def __init__(self, logic_contract_address, certifier):
         self.init_type(
             sp.TRecord(
-                mapping = sp.TBigMap(
+                schema_map = sp.TBigMap(
                     sp.TString,
                     sp.TRecord(
                         schema_data = sp.TString,
-                        status = sp.TString
+                        status = sp.TBool
                     )
                 ),
+                logic_contract_address = sp.TAddress,
+                certifier = sp.TAddress
             )
+        )
+        self.init(
+            schema_map = sp.big_map(),
+            logic_contract_address = logic_contract_address,
+            certifier = certifier
         )
 
     @sp.entry_point
     def add(self, schema_id, schema_data):
-        self.update_initial_storage(mapping = sp.big_map())
         sp.set_type(schema_id, sp.TString)
         sp.set_type(schema_data, sp.TString)
+
+        data = sp.TRecord(schema_id = sp.TString, schema_data = sp.TString, status = sp.TBool)
+        contract_data = sp.TRecord(contract = sp.TContract(data), schema_id = sp.TString, schema_data = sp.TString)
+        
+        logic_contract = sp.contract(contract_data, self.data.logic_contract_address, "add").open_some()
+        
+        params = sp.record(
+            contract = sp.self_entry_point("store"),
+            schema_id = schema_id,
+            schema_data = schema_data
+        )
+
+        sp.transfer(params, sp.mutez(0), logic_contract)
+    
+    @sp.entry_point
+    def store(self, params):
+        data = sp.record(
+            schema_data = params.schema_data,
+            status = params.status
+        )
+
+        self.data.schema_map[params.schema_id] = data
+
+    @sp.onchain_view()
+    def get(self, schema_id):
+        sp.result(self.data.schema_map[schema_id])
         
     @sp.entry_point
     def revoke(self, schema_id):
-        self.update_initial_storage(mapping = sp.big_map())
+        self.update_initial_storage(schema_map = sp.big_map())
         sp.set_type(schema_id, sp.TString)
 
     @sp.entry_point
-    def get_data(self, schema_id):
-        self.update_initial_storage(mapping = sp.big_map())
-        sp.set_type(schema_id, sp.TString)
+    def change_logic_contract_address(self, new_logic_contract_address):
+        with sp.if_(self.data.certifier != sp.source):
+            sp.failwith("Incorrect certifier")
+        
+        self.data.logic_contract_address = new_logic_contract_address
 
 @sp.add_test(name = "SchemaRegistry")
 def test():
     
-    sp.add_compilation_target("schemaRegistry", SchemaRegistry('schema_id', 'logic_contract_address'))
+    sp.add_compilation_target("schemaRegistry", SchemaRegistry(sp.address('KT1CiaJUoi7GZfZ8tWgpmJJ683QutdUfM9Yf'), sp.address('tz1PvnsRQsdhfEUFdFg6Z4CQpeDhWbXEDswm')))
--- schemaRegistryLogic.py
+++ schemaRegistryLogic.py
@@ -3,21 +3,22 @@
 import smartpy as sp
 
 class SchemaRegistryLogic(sp.Contract):
-    def __init__(self, schema_id):
-        self.init_type(
-            sp.TRecord(
-                schema_id = sp.TString
-            )
-        )
-        self.init(
-            schema_id = schema_id
-        )
+    def __init__(self):
+        self.init();
 
     @sp.entry_point
-    def add(self, schema_id, schema_data):
-        sp.set_type(schema_id, sp.TString)
-        sp.set_type(schema_data, sp.TString)
+    def add(self, input_data):
+        sp.set_type(input_data.schema_id, sp.TString)
+        sp.set_type(input_data.schema_data, sp.TString)
         
+        output_data = sp.record(
+            schema_id = input_data.schema_id,
+            schema_data = input_data.schema_data,
+            status = sp.bool(True)
+        )
+
+        sp.transfer(output_data, sp.mutez(0), input_data.contract)
+    
     @sp.entry_point
     def revoke(self, schema_id):
         sp.set_type(schema_id, sp.TString)
@@ -25,4 +26,4 @@ class SchemaRegistryLogic(sp.Contract):
 @sp.add_test(name = "SchemaRegistryLogic")
 def test():
     
-    sp.add_compilation_target("schemaRegistryLogic", SchemaRegistryLogic('schema_id'))
+    sp.add_compilation_target("schemaRegistryLogic", SchemaRegistryLogic())
